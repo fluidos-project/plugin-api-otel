6e0b4e1a8a60339617090f4d0e8eb5b59bc8292d
JoseMJuni <josemanuel.bernabe@gmail.com>
2025-04-07 17:40:06 +0200

    update

--- agent.py
+++ agent.py
@@ -16,6 +16,10 @@ class OTELConfiguration(BaseModel):
     processors: Dict[str, Any]
     exporters: Dict[str, Any]
     service: Dict[str, Any]
+    
+class OTELReload(BaseModel):
+    namespace: Any
+    label_selector: Any
 
 # Load Kubernetes configuration
 def load_kubernetes_config():
@@ -29,20 +33,32 @@ def load_kubernetes_config():
 
 # Update receiver structure
 def __update_configmap(new_configmap: dict, configmap: dict): 
-    for key, value in new_configmap.items():
-        if key in configmap and isinstance(value, dict):
-            # Check level matches
-            __update_configmap(value, configmap[key])
-        else:
-            configmap[key] = value
-        
+    try:
+        for key, value in new_configmap.items():
+            if key in configmap and isinstance(value, dict):
+                # Check level matches
+                __update_configmap(value, configmap[key])
+            else:
+                configmap[key] = value
+    except Exception as e:
+        print(f"Error updating configmap: {e}")
+        raise HTTPException(status_code=500, detail=f"Fail to update configmap: {e}")
+            
 # Delete keys from the configmap 
 def __remove_configmap(keys_to_remove: dict, configmap: dict):
-    for key, value in keys_to_remove.items():
-        if key in configmap and  key != 'pipelines':
-            del configmap[key]
-        elif  isinstance(value, dict):
-            __remove_configmap(value, configmap[key])
+    try:
+        for key, value in keys_to_remove.items():
+            if key in configmap:
+                if isinstance(value, dict) and isinstance(configmap[key], dict):
+                    __remove_configmap(value, configmap[key])
+                elif isinstance(value, list) and isinstance(configmap[key], list):
+                    configmap[key] = [item for item in configmap[key] if item not in value]
+                elif configmap[key] == value:
+                    del configmap[key]
+    except Exception as e:
+        print(f"Error removing configmap: {e}")
+        raise HTTPException(status_code=500, detail=f"Fail to remove configmap: {e}")
+
             
 def __load_test_configmap():
     with open("test/template.yaml") as file:
@@ -50,6 +66,20 @@ def __load_test_configmap():
             return yaml.safe_load(file) 
         except yaml.YAMLError as exc:
             print(exc)
+            
+def __clean_empty(d):
+    """
+    Clean empty values from a dictionary.
+    """
+    if not isinstance(d, dict):
+        return d
+
+    return {
+        k: __clean_empty(v)
+        for k, v in d.items()
+        if v not in (None, {}, [], '') and not (isinstance(v, dict) and not __clean_empty(v))
+    }
+
 
 # Update the ConfigMap with the new pipeline configuration
 def update_configmap(namespace: str, configmap_name: str, new_pipeline: dict):
@@ -57,16 +87,15 @@ def update_configmap(namespace: str, configmap_name: str, new_pipeline: dict):
         load_kubernetes_config()
         v1 = client.CoreV1Api()
         # Retrieve the current ConfigMap
-        configmap_yaml = v1.read_namespaced_config_map(configmap_name, namespace)
-        print(configmap_yaml.data)
-        configmap      = yaml.safe_load(configmap_yaml.data['collector.yaml'])
+        configmap           = v1.read_namespaced_config_map(configmap_name, namespace)
+        configmap_yaml      = yaml.safe_load(configmap.data['collector.yaml'])
     except Exception as e:
         if configmap_name is None:
             print("No Kubeconfig, entering DEBUG mode")
-            configmap = yaml.safe_load(__load_test_configmap()['data']['collector.yaml'])
-            print(f"TESTING: {configmap}")
+            configmap_yaml = yaml.safe_load(__load_test_configmap()['data']['collector.yaml'])
+            print(f"TESTING: {configmap_yaml}")
         else:
-            raise Exception(status_code=500, detail=f"Fail to load Kubeconfig: {e}")
+            raise HTTPException(status_code=500, detail=f"Fail to load Kubeconfig: {e}")
 
     # Extract pipeline details
     receiver    = new_pipeline['receivers']  if 'receivers' in new_pipeline else None
@@ -74,18 +103,19 @@ def update_configmap(namespace: str, configmap_name: str, new_pipeline: dict):
     exporter    = new_pipeline['exporters']  if 'exporters' in new_pipeline else None 
     service     = new_pipeline['service']    if 'service' in new_pipeline else None
     
-    updates_new_configmaps = [receiver, processor, exporter, service]
-    
+    updates_new_configmaps = [(receiver, 'receivers'), (processor, 'processors'), (exporter, 'exporters'), (service, 'service')]
     for new_configmap in updates_new_configmaps:
-        __update_configmap(new_configmap, configmap['receivers'])
+        __update_configmap(new_configmap[0], configmap_yaml[new_configmap[1]])
     if configmap_name is not None:
         try:
+            updated_yaml = yaml.safe_dump(configmap_yaml)
+            configmap.data['collector.yaml'] = updated_yaml
             v1.replace_namespaced_config_map(configmap_name, namespace, configmap)
             return configmap
         except Exception as e:
-            raise Exception(status_code=500, detail=f"Fail to replace configmap: {e}")
+            raise HTTPException(status_code=500, detail=f"Fail to replace configmap: {e}")
     else:
-        return configmap
+        return configmap_yaml
         
         
         
@@ -95,22 +125,28 @@ def add_configmap(namespace: str, configmap_name: str, new_configmap: dict):
         load_kubernetes_config()
         v1 = client.CoreV1Api()
         # Retrieve the current ConfigMap
-        configmap_yaml = v1.read_namespaced_config_map(configmap_name, namespace)
-        configmap      = yaml.safe_load(configmap_yaml.data['collector.yaml'])
+        configmap           = v1.read_namespaced_config_map(configmap_name, namespace)
+        configmap_yaml      = yaml.safe_load(configmap.data['collector.yaml'])
     except Exception as e:
         if configmap_name is None:
             print("No Kubeconfig, entering DEBUG mode")
-            configmap = yaml.safe_load(__load_test_configmap()['data']['collector.yaml'])
-            print(f"TESTING: {configmap}")
-            configmap = new_configmap
-            return configmap
+            configmap_yaml = yaml.safe_load(__load_test_configmap()['data']['collector.yaml'])
+            print(f"TESTING: {configmap_yaml}")
+            configmap_yaml = new_configmap
+            return configmap_yaml
         else:
-            raise Exception(status_code=500, detail=f"Fail to load Kubeconfig: {e}")
+            raise HTTPException(status_code=500, detail=f"Fail to load Kubeconfig: {e}")
 
-    try:
-        v1.replace_namespaced_config_map(configmap_name, namespace, configmap)
-    except Exception as e:
-        raise Exception(status_code=500, detail=f"Fail to replace configmap: {e}")
+    if configmap_name is not None:
+        try:
+            updated_yaml = yaml.safe_dump(configmap_yaml)
+            configmap.data['collector.yaml'] = updated_yaml
+            v1.replace_namespaced_config_map(configmap_name, namespace, configmap)
+            return configmap
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Fail to replace configmap: {e}")
+    else:
+        return configmap_yaml
 
         
         
@@ -119,15 +155,15 @@ def remove_configmap(namespace: str, configmap_name: str, remove_pipeline: dict)
         load_kubernetes_config()
         v1 = client.CoreV1Api()
         # Retrieve the current ConfigMap
-        configmap_yaml = v1.read_namespaced_config_map(configmap_name, namespace)
-        configmap      = yaml.safe_load(configmap_yaml.data['collector.yaml'])
+        configmap           = v1.read_namespaced_config_map(configmap_name, namespace)
+        configmap_yaml      = yaml.safe_load(configmap.data['collector.yaml'])
     except Exception as e:
         if configmap_name is None:
             print("No Kubeconfig, entering DEBUG mode")
-            configmap = yaml.safe_load(__load_test_configmap()['data']['collector.yaml'])
-            print(f"TESTING: {configmap}")
+            configmap_yaml = yaml.safe_load(__load_test_configmap()['data']['collector.yaml'])
+            print(f"TESTING: {configmap_yaml}")
         else:
-            raise Exception(status_code=500, detail=f"Fail to load Kubeconfig: {e}")
+            raise HTTPException(status_code=500, detail=f"Fail to load Kubeconfig: {e}")
 
     # Extract pipeline details
     receiver    = remove_pipeline['receivers']  if 'receivers' in remove_pipeline else dict()
@@ -137,7 +173,19 @@ def remove_configmap(namespace: str, configmap_name: str, remove_pipeline: dict)
     
     update_remove_configmaps = [(receiver, 'receivers'), (processor, 'processors'), (exporter, 'exporters'), (service, 'service')]
     for new_configmap in update_remove_configmaps:
-        __remove_configmap(new_configmap[0], configmap[new_configmap[1]])
+        __remove_configmap(new_configmap[0], configmap_yaml[new_configmap[1]])
+    configmap_yaml = __clean_empty(configmap_yaml)
+    if configmap_name is not None:
+        try:
+            updated_yaml = yaml.safe_dump(configmap_yaml)
+            configmap.data['collector.yaml'] = updated_yaml
+            v1.replace_namespaced_config_map(configmap_name, namespace, configmap)
+            return configmap
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Fail to replace configmap: {e}")
+    else:
+        return configmap_yaml
+
 
 
 # List all pods in a namespace
@@ -195,7 +243,7 @@ def update_pipeline(request: OTELConfiguration):
         # Update the ConfigMap with the new pipeline
         new_configmap = update_configmap(namespace, configmap_name, request.model_dump())
 
-        return {"message": "Pipeline created and ConfigMap successfully updated", "configmap": new_configmap}
+        return {"message": "Pipeline created and ConfigMap successfully updated"}
     except Exception as e:
         print(f"Error updating ConfigMap: {e}")
         raise HTTPException(status_code=500, detail=str(e))
@@ -211,7 +259,7 @@ def add_pipeline(request: OTELConfiguration):
         # Update the ConfigMap with the new pipeline
         new_configmap = add_configmap(namespace, configmap_name, request.model_dump())
 
-        return {"message": "Pipeline created and ConfigMap successfully updated", "configmap": new_configmap}
+        return {"message": "Pipeline created and ConfigMap successfully updated"}
     except Exception as e:
         print(f"Error updating ConfigMap: {e}")
         raise HTTPException(status_code=500, detail=str(e))
@@ -225,8 +273,9 @@ def remove_pipeline(request: OTELConfiguration):
         configmap_name = data['configmap_name']
 
         # Update the ConfigMap with the new pipeline
-        remove_configmap(namespace, configmap_name, request.model_dump())
+        configmap = remove_configmap(namespace, configmap_name, request.model_dump())
 
+        # return {"message": "Pipeline deleted and ConfigMap successfully updated. \n", "configmap": configmap}
         return {"message": "Pipeline deleted and ConfigMap successfully updated."}
     except Exception as e:
         print(f"Error updating ConfigMap: {e}")
@@ -248,9 +297,7 @@ def list_pipelines():
         configmap_yaml = yaml.safe_load(configmap.data['collector.yaml'])
         
         # Extract the pipelines
-        pipelines = configmap_yaml.get("service", {}).get("pipelines", {})
-        
-        return {"pipelines": list(pipelines.keys())}
+        return configmap_yaml
     except Exception as e:
         print(f"Error listing pipelines: {e}")
         raise HTTPException(status_code=500, detail=str(e))
@@ -258,10 +305,10 @@ def list_pipelines():
 
 # Endpoint to reload the OpenTelemetry Collector configuration
 @app.post("/reload")
-def reload_config(request: OTELConfiguration):
+def reload_config(request: OTELReload):
     try:
         namespace = request.model_dump()['namespace']
-        label_selector = "app.kubernetes.io/name=opentelemetrycollector"
+        label_selector =  request.model_dump()['label_selector'] if  request.model_dump()['namespace'] is not None else "app.kubernetes.io/name=opentelemetrycollector"
 
         # Find the OpenTelemetry pod by its label
         pod_name = find_pod_by_label(namespace, label_selector)
@@ -279,4 +326,4 @@ def reload_config(request: OTELConfiguration):
 # Run the FastAPI app with Uvicorn
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("agent:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("agent:app", host="0.0.0.0", port=8000, reload=True, workers=1)
